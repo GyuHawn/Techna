bad396dea20c6e52bf1921b74df397f07bff55d5
GyuHawn <knm128882@gmail.com>
2024-07-23 23:13:09 +0900

    총알속성 선택 기능 추가, 선택한 총알 속성에 따른 조합 기능 추가

--- Puzzle/Assets/Scripts/Gem/GemCombination.cs
+++ Puzzle/Assets/Scripts/Gem/GemCombination.cs
@@ -1,20 +1,234 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.ShortcutManagement;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class GemCombination : MonoBehaviour
 {
+    public int selectBulletNum;
+    public int selectAttributeNum;
+    public int selectFunctionNum;
+    public GameObject[] B_Gems; // ÃÑ¾Ë
+    public GameObject[] B_A_Gems; // ÃÑ¾Ë + ¼Ó¼º
+    public GameObject[] B_F_Gems; // ÃÑ¾Ë + ±â´É
+    public GameObject[] B_A_F_Gems; // ÃÑ¾Ë + ¼Ó¼º + ±â´É
 
     public Sprite[] crossHair;
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        SelectGem(selectBulletNum, selectAttributeNum, selectFunctionNum);
+    }
+
+    void ResetGem()
+    {
+        foreach(var gem in B_Gems)
+        {
+            gem.SetActive(false);
+        }
+        foreach (var gem in B_A_Gems)
+        {
+            gem.SetActive(false);
+        }
+        foreach (var gem in B_F_Gems)
+        {
+            gem.SetActive(false);
+        }
+        foreach (var gem in B_A_F_Gems)
+        {
+            gem.SetActive(false);
+        }
+    }
+
+    void SelectGem(int bullet, int attribute, int function)
+    {
+        ResetGem();
+        switch (bullet, attribute, function)
+        {
+            // ±âº»Åº
+            case (1, 0, 0):
+                B_Gems[0].SetActive(true);
+                break;
+            case (1, 1, 0):
+                B_A_Gems[0].SetActive(true);
+                break;
+            case (1, 2, 0):
+                B_A_Gems[1].SetActive(true);
+                break;
+            case (1, 3, 0):
+                B_A_Gems[2].SetActive(true);
+                break;
+            case (1, 4, 0):
+                B_A_Gems[3].SetActive(true);
+                break;
+            case (1, 0, 1):
+                B_F_Gems[0].SetActive(true);
+                break;
+            case (1, 0, 2):
+                B_F_Gems[1].SetActive(true);
+                break;
+            case (1, 0, 3):
+                B_F_Gems[2].SetActive(true);
+                break;
+            case (1, 0, 4):
+                B_F_Gems[3].SetActive(true);
+                break;
+            case (1, 0, 5):
+                B_F_Gems[4].SetActive(true);
+                break;
+            case (1, 1, 1):
+                B_A_F_Gems[0].SetActive(true);
+                break;
+            case (1, 1, 2):
+                B_A_F_Gems[1].SetActive(true);
+                break;
+            case (1, 1, 3):
+                B_A_F_Gems[2].SetActive(true);
+                break;
+            case (1, 1, 4):
+                B_A_F_Gems[3].SetActive(true);
+                break;
+            case (1, 1, 5):
+                B_A_F_Gems[4].SetActive(true);
+                break;
+            case (1, 2, 1):
+                B_A_F_Gems[5].SetActive(true);
+                break;
+            case (1, 2, 2):
+                B_A_F_Gems[6].SetActive(true);
+                break;
+            case (1, 2, 3):
+                B_A_F_Gems[7].SetActive(true);
+                break;
+            case (1, 2, 4):
+                B_A_F_Gems[8].SetActive(true);
+                break;
+            case (1, 2, 5):
+                B_A_F_Gems[9].SetActive(true);
+                break;
+            case (1, 3, 1):
+                B_A_F_Gems[10].SetActive(true);
+                break;
+            case (1, 3, 2):
+                B_A_F_Gems[11].SetActive(true);
+                break;
+            case (1, 3, 3):
+                B_A_F_Gems[12].SetActive(true);
+                break;
+            case (1, 3, 4):
+                B_A_F_Gems[13].SetActive(true);
+                break;
+            case (1, 3, 5):
+                B_A_F_Gems[14].SetActive(true);
+                break;
+            case (1, 4, 1):
+                B_A_F_Gems[15].SetActive(true);
+                break;
+            case (1, 4, 2):
+                B_A_F_Gems[16].SetActive(true);
+                break;
+            case (1, 4, 3):
+                B_A_F_Gems[17].SetActive(true);
+                break;
+            case (1, 4, 4):
+                B_A_F_Gems[18].SetActive(true);
+                break;
+            case (1, 4, 5):
+                B_A_F_Gems[19].SetActive(true);
+                break;
+
+            // ´ëÇüÅº
+            case (2, 0, 0):
+                B_Gems[1].SetActive(true);
+                break;
+            case (2, 1, 0):
+                B_A_Gems[4].SetActive(true);
+                break;
+            case (2, 2, 0):
+                B_A_Gems[5].SetActive(true);
+                break;
+            case (2, 3, 0):
+                B_A_Gems[6].SetActive(true);
+                break;
+            case (2, 4, 0):
+                B_A_Gems[7].SetActive(true);
+                break;
+            case (2, 0, 1):
+                B_F_Gems[5].SetActive(true);
+                break;
+            case (2, 0, 2):
+                B_F_Gems[6].SetActive(true);
+                break;
+            case (2, 0, 3):
+                B_F_Gems[7].SetActive(true);
+                break;
+            case (2, 0, 4):
+                B_F_Gems[8].SetActive(true);
+                break;
+            case (2, 0, 5):
+                B_F_Gems[9].SetActive(true);
+                break;
+            case (2, 1, 1):
+                B_A_F_Gems[20].SetActive(true);
+                break;
+            case (2, 1, 2):
+                B_A_F_Gems[21].SetActive(true);
+                break;
+            case (2, 1, 3):
+                B_A_F_Gems[22].SetActive(true);
+                break;
+            case (2, 1, 4):
+                B_A_F_Gems[23].SetActive(true);
+                break;
+            case (2, 1, 5):
+                B_A_F_Gems[24].SetActive(true);
+                break;
+            case (2, 2, 1):
+                B_A_F_Gems[25].SetActive(true);
+                break;
+            case (2, 2, 2):
+                B_A_F_Gems[26].SetActive(true);
+                break;
+            case (2, 2, 3):
+                B_A_F_Gems[27].SetActive(true);
+                break;
+            case (2, 2, 4):
+                B_A_F_Gems[28].SetActive(true);
+                break;
+            case (2, 2, 5):
+                B_A_F_Gems[29].SetActive(true);
+                break;
+            case (2, 3, 1):
+                B_A_F_Gems[30].SetActive(true);
+                break;
+            case (2, 3, 2):
+                B_A_F_Gems[31].SetActive(true);
+                break;
+            case (2, 3, 3):
+                B_A_F_Gems[32].SetActive(true);
+                break;
+            case (2, 3, 4):
+                B_A_F_Gems[33].SetActive(true);
+                break;
+            case (2, 3, 5):
+                B_A_F_Gems[34].SetActive(true);
+                break;
+            case (2, 4, 1):
+                B_A_F_Gems[35].SetActive(true);
+                break;
+            case (2, 4, 2):
+                B_A_F_Gems[36].SetActive(true);
+                break;
+            case (2, 4, 3):
+                B_A_F_Gems[37].SetActive(true);
+                break;
+            case (2, 4, 4):
+                B_A_F_Gems[38].SetActive(true);
+                break;
+            case (2, 4, 5):
+                B_A_F_Gems[39].SetActive(true);
+                break;
+        }
     }
 }
--- Puzzle/Assets/Scripts/Gem/GemUI.cs
+++ Puzzle/Assets/Scripts/Gem/GemUI.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GemUI : MonoBehaviour
@@ -12,11 +13,10 @@ public class GemUI : MonoBehaviour
     public GameObject attributeMenuUI; // 속성 메뉴 UI
     public GameObject functionUI; // 기능 보석
     public GameObject functionMenuUI; // 기능 메뉴 UI
+    public int selectGemNum;
 
     // 각 활성화 여부
-    private bool onBulletUI;
-    private bool onAttributeUI;
-    private bool onFunctionUI;
+    private bool selectBullet;
 
     private void Awake()
     {
@@ -26,39 +26,106 @@ public class GemUI : MonoBehaviour
 
     void Update()
     {
-        // 키 입력에 따른 UI 열고 닫기
-        OpenGemUI("1", ref onBulletUI, bulletMenuUI);
-        OpenGemUI("2", ref onAttributeUI, attributeMenuUI);
-        OpenGemUI("3", ref onFunctionUI, functionMenuUI);
+        if (!selectBullet) // 총알, 속성, 기능 메뉴 중 선택
+        {
+            // 키 입력에 따른 UI 열고 닫기
+            OpenGemUI("1", bulletMenuUI, 1); // 1번키, 메뉴, 선택값
+            OpenGemUI("2", attributeMenuUI, 2);
+            OpenGemUI("3", functionMenuUI, 3);
+        }
+        else if(selectBullet) // 선택한 메뉴 안에서 선택지 선택
+        {
+            SelectFunction(); // 기능 선택
+        }
     }
 
-    private void OpenGemUI(string KeyNum, ref bool uiState, GameObject menuUI) // 키 입력에 따른 UI 열고 닫기
+    private void OpenGemUI(string KeyNum, GameObject menuUI, int menuNum) // 키 입력에 따른 UI 열고 닫기
     {
         if (Input.GetButtonDown(KeyNum)) // 키입력 시
         {
-            if (!uiState) // 활성화 상태 확인
+            selectBullet = true;
+            ResetUIStates(); // 모든 UI 상태 초기화 및 닫기
+            menuUI.SetActive(true); // 메뉴 열기
+            selectGemNum = menuNum; // 선택한 메뉴 값
+        }
+    }
+
+    private void SelectFunction() // 기능 선택
+    {
+        if (selectGemNum == 1) // 총알 메뉴
+        {
+            SelectFunctionValue(1, ref gemCombination.selectBulletNum, 2); // 선택 메뉴, 전달할 변수, 선택지
+        }
+        else if (selectGemNum == 2) // 속성 메뉴
+        {
+            SelectFunctionValue(2, ref gemCombination.selectAttributeNum, 4);
+        }
+        else if (selectGemNum == 3) // 기능 메뉴
+        {
+            SelectFunctionValue(3, ref gemCombination.selectFunctionNum, 5);
+        }
+    }
+
+    private void SelectFunctionValue(int menuNum, ref int selectedFunction, int choiceNum) // 메뉴 내 선택지 처리
+    {
+        if (menuNum == 1)
+        {
+            for (int i = 1; i <= choiceNum; i++)
             {
-                ResetUIStates(); // 모든 UI 상태 초기화 및 닫기
-                uiState = true; // 상태 활성화
-                menuUI.SetActive(true); // 메뉴 열기
+                if (Input.GetKeyDown(KeyCode.Alpha1 + (i - 1)))
+                {
+                    selectedFunction = i;
+                    ConfirmSelection();
+                    return;
+                }
             }
-            else
+        }
+        else if(menuNum == 2)
+        {
+            for (int i = 1; i <= choiceNum; i++)
             {
-                uiState = false;
-                menuUI.SetActive(false);
+                if (Input.GetKeyDown(KeyCode.Alpha1 + (i - 1)))
+                {
+                    selectedFunction = i;
+                    ConfirmSelection();
+                    return;
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha1 + choiceNum)) // 마지막 선택지를 0으로 처리
+            {
+                selectedFunction = 0;
+                ConfirmSelection();
+            }
+        }
+        else if(menuNum == 3) 
+        {
+            for (int i = 1; i <= choiceNum; i++)
+            {
+                if (Input.GetKeyDown(KeyCode.Alpha1 + (i - 1)))
+                {
+                    selectedFunction = i;
+                    ConfirmSelection();
+                    return;
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha1 + choiceNum)) // 마지막 선택지를 0으로 처리
+            {
+                selectedFunction = 0;
+                ConfirmSelection();
             }
         }
     }
 
+    private void ConfirmSelection() // 선택 확인 처리
+    {
+        selectBullet = false;
+        ResetUIStates();
+    }
+
     private void ResetUIStates()  // 모든 UI 상태 초기화 및 닫기
     {
-        onBulletUI = false; 
         bulletMenuUI.SetActive(false);
-
-        onAttributeUI = false;
         attributeMenuUI.SetActive(false);
-
-        onFunctionUI = false;
         functionMenuUI.SetActive(false);
     }
 }
