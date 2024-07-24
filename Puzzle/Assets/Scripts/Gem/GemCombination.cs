1674c8b4a7935447ed5885dcc8502342536c25bb
GyuHawn <knm128882@gmail.com>
2024-07-24 15:39:31 +0900

    총알 속성 조합기능 완료, 보석 사이즈 조정

--- Puzzle/Assets/Scripts/Gem/GemCombination.cs
+++ Puzzle/Assets/Scripts/Gem/GemCombination.cs
@@ -6,229 +6,79 @@ using UnityEngine.UI;
 
 public class GemCombination : MonoBehaviour
 {
-    public int selectBulletNum;
-    public int selectAttributeNum;
-    public int selectFunctionNum;
+    public int selectBulletNum; // 선택된 총알 값
+    public int selectAttributeNum; // 선택된 속성 값
+    public int selectFunctionNum; // 선택된 기능 값
     public GameObject[] B_Gems; // 총알
     public GameObject[] B_A_Gems; // 총알 + 속성
     public GameObject[] B_F_Gems; // 총알 + 기능
     public GameObject[] B_A_F_Gems; // 총알 + 속성 + 기능
+    public float currentGemNum; // 현재 선택된 보석 값
 
-    public Sprite[] crossHair;
+    public Sprite[] crossHair; // 조준점 배열
 
     void Update()
     {
+        // 선택한 보석
         SelectGem(selectBulletNum, selectAttributeNum, selectFunctionNum);
     }
 
-    void ResetGem()
+    void ResetGem() // 모든 보석 비활성화
     {
-        foreach(var gem in B_Gems)
-        {
-            gem.SetActive(false);
-        }
-        foreach (var gem in B_A_Gems)
-        {
-            gem.SetActive(false);
-        }
-        foreach (var gem in B_F_Gems)
-        {
-            gem.SetActive(false);
-        }
-        foreach (var gem in B_A_F_Gems)
+        DeactivateAllGems(B_Gems);
+        DeactivateAllGems(B_A_Gems);
+        DeactivateAllGems(B_F_Gems);
+        DeactivateAllGems(B_A_F_Gems);
+    }
+    void DeactivateAllGems(GameObject[] gems) // 보석 비활성화
+    {
+        foreach (var gem in gems)
         {
             gem.SetActive(false);
         }
     }
 
-    void SelectGem(int bullet, int attribute, int function)
+    void SelectGem(int bullet, int attribute, int function) // 보석 선택
     {
-        ResetGem();
-        switch (bullet, attribute, function)
-        {
-            // 기본탄
-            case (1, 0, 0):
-                B_Gems[0].SetActive(true);
-                break;
-            case (1, 1, 0):
-                B_A_Gems[0].SetActive(true);
-                break;
-            case (1, 2, 0):
-                B_A_Gems[1].SetActive(true);
-                break;
-            case (1, 3, 0):
-                B_A_Gems[2].SetActive(true);
-                break;
-            case (1, 4, 0):
-                B_A_Gems[3].SetActive(true);
-                break;
-            case (1, 0, 1):
-                B_F_Gems[0].SetActive(true);
-                break;
-            case (1, 0, 2):
-                B_F_Gems[1].SetActive(true);
-                break;
-            case (1, 0, 3):
-                B_F_Gems[2].SetActive(true);
-                break;
-            case (1, 0, 4):
-                B_F_Gems[3].SetActive(true);
-                break;
-            case (1, 0, 5):
-                B_F_Gems[4].SetActive(true);
-                break;
-            case (1, 1, 1):
-                B_A_F_Gems[0].SetActive(true);
-                break;
-            case (1, 1, 2):
-                B_A_F_Gems[1].SetActive(true);
-                break;
-            case (1, 1, 3):
-                B_A_F_Gems[2].SetActive(true);
-                break;
-            case (1, 1, 4):
-                B_A_F_Gems[3].SetActive(true);
-                break;
-            case (1, 1, 5):
-                B_A_F_Gems[4].SetActive(true);
-                break;
-            case (1, 2, 1):
-                B_A_F_Gems[5].SetActive(true);
-                break;
-            case (1, 2, 2):
-                B_A_F_Gems[6].SetActive(true);
-                break;
-            case (1, 2, 3):
-                B_A_F_Gems[7].SetActive(true);
-                break;
-            case (1, 2, 4):
-                B_A_F_Gems[8].SetActive(true);
-                break;
-            case (1, 2, 5):
-                B_A_F_Gems[9].SetActive(true);
-                break;
-            case (1, 3, 1):
-                B_A_F_Gems[10].SetActive(true);
-                break;
-            case (1, 3, 2):
-                B_A_F_Gems[11].SetActive(true);
-                break;
-            case (1, 3, 3):
-                B_A_F_Gems[12].SetActive(true);
-                break;
-            case (1, 3, 4):
-                B_A_F_Gems[13].SetActive(true);
-                break;
-            case (1, 3, 5):
-                B_A_F_Gems[14].SetActive(true);
-                break;
-            case (1, 4, 1):
-                B_A_F_Gems[15].SetActive(true);
-                break;
-            case (1, 4, 2):
-                B_A_F_Gems[16].SetActive(true);
-                break;
-            case (1, 4, 3):
-                B_A_F_Gems[17].SetActive(true);
-                break;
-            case (1, 4, 4):
-                B_A_F_Gems[18].SetActive(true);
-                break;
-            case (1, 4, 5):
-                B_A_F_Gems[19].SetActive(true);
-                break;
+        ResetGem(); // 모든 보석 비활성화
 
-            // 대형탄
-            case (2, 0, 0):
-                B_Gems[1].SetActive(true);
-                break;
-            case (2, 1, 0):
-                B_A_Gems[4].SetActive(true);
-                break;
-            case (2, 2, 0):
-                B_A_Gems[5].SetActive(true);
-                break;
-            case (2, 3, 0):
-                B_A_Gems[6].SetActive(true);
-                break;
-            case (2, 4, 0):
-                B_A_Gems[7].SetActive(true);
-                break;
-            case (2, 0, 1):
-                B_F_Gems[5].SetActive(true);
-                break;
-            case (2, 0, 2):
-                B_F_Gems[6].SetActive(true);
-                break;
-            case (2, 0, 3):
-                B_F_Gems[7].SetActive(true);
-                break;
-            case (2, 0, 4):
-                B_F_Gems[8].SetActive(true);
-                break;
-            case (2, 0, 5):
-                B_F_Gems[9].SetActive(true);
-                break;
-            case (2, 1, 1):
-                B_A_F_Gems[20].SetActive(true);
-                break;
-            case (2, 1, 2):
-                B_A_F_Gems[21].SetActive(true);
-                break;
-            case (2, 1, 3):
-                B_A_F_Gems[22].SetActive(true);
-                break;
-            case (2, 1, 4):
-                B_A_F_Gems[23].SetActive(true);
-                break;
-            case (2, 1, 5):
-                B_A_F_Gems[24].SetActive(true);
-                break;
-            case (2, 2, 1):
-                B_A_F_Gems[25].SetActive(true);
-                break;
-            case (2, 2, 2):
-                B_A_F_Gems[26].SetActive(true);
-                break;
-            case (2, 2, 3):
-                B_A_F_Gems[27].SetActive(true);
-                break;
-            case (2, 2, 4):
-                B_A_F_Gems[28].SetActive(true);
-                break;
-            case (2, 2, 5):
-                B_A_F_Gems[29].SetActive(true);
-                break;
-            case (2, 3, 1):
-                B_A_F_Gems[30].SetActive(true);
-                break;
-            case (2, 3, 2):
-                B_A_F_Gems[31].SetActive(true);
-                break;
-            case (2, 3, 3):
-                B_A_F_Gems[32].SetActive(true);
-                break;
-            case (2, 3, 4):
-                B_A_F_Gems[33].SetActive(true);
-                break;
-            case (2, 3, 5):
-                B_A_F_Gems[34].SetActive(true);
-                break;
-            case (2, 4, 1):
-                B_A_F_Gems[35].SetActive(true);
-                break;
-            case (2, 4, 2):
-                B_A_F_Gems[36].SetActive(true);
-                break;
-            case (2, 4, 3):
-                B_A_F_Gems[37].SetActive(true);
-                break;
-            case (2, 4, 4):
-                B_A_F_Gems[38].SetActive(true);
-                break;
-            case (2, 4, 5):
-                B_A_F_Gems[39].SetActive(true);
-                break;
+        int gemIndex = GetGemIndex(bullet, attribute, function); // 보석 인덱스 계산
+        currentGemNum = CalculateCurrentGem(bullet, attribute, function); // 선택한 보석 값 설정
+
+        if (bullet == 1) // 기본탄
+        {
+            if (attribute == 0 && function == 0) // 속성x, 기능x
+                B_Gems[0].SetActive(true);
+            else if (attribute == 0) // 속성x, 기능o
+                B_F_Gems[function - 1].SetActive(true);
+            else if (function == 0) // 기능x, 속성o
+                B_A_Gems[attribute - 1].SetActive(true);
+            else // 속성o, 기능o
+                B_A_F_Gems[gemIndex].SetActive(true);
         }
+        else if (bullet == 2) // 대형탄
+        {
+            if (attribute == 0 && function == 0) // 속성x, 기능x
+                B_Gems[1].SetActive(true);
+            else if (attribute == 0) // 속성x, 기능o
+                B_F_Gems[function + 4].SetActive(true);
+            else if (function == 0) // 기능x, 속성o
+                B_A_Gems[attribute + 3].SetActive(true);
+            else // 속성o, 기능o
+                B_A_F_Gems[gemIndex + 20].SetActive(true);
+        }
+    }
+
+    int GetGemIndex(int bullet, int attribute, int function) // 보석 인덱스 계산
+    {
+        return (attribute - 1) * 5 + (function - 1);
+    }
+
+    float CalculateCurrentGem(int bullet, int attribute, int function) // 선택한 보석 값 설정
+    {
+        float baseValue = bullet;
+        float attributeValue = attribute * 0.1f;
+        float functionValue = function * 0.01f;
+        return baseValue + attributeValue + functionValue;
     }
 }
