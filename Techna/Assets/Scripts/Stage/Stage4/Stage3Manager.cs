306cfc65bab29d85abd1775fdef6efbf94e9ff00
GyuHawn <knm128882@gmail.com>
2024-12-20 22:53:33 +0900

    몬스터 소환

--- Techna/Assets/Scripts/Stage/Stage4/Stage3Manager.cs
+++ Techna/Assets/Scripts/Stage/Stage4/Stage3Manager.cs
@@ -1,46 +1,141 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Stage3Manager : MonoBehaviour
 {
     public SummonManager summonManager;
 
+    // 몬스터 소환 위치
     public Transform[] creepSummonPositions;
     public Transform[] demonSummonPositions;
 
-    public int wave;
-    public int creepCount;
+    public int wave; // 웨이브
+    public float gameTime; // 시간
+    public int creepCount; // 소환할 몬스터 수
     public int demonCount;
+    public int totalMonsterCount; // 전체 몬스터 수
 
-    private void Start()
+    public bool start; // 시작 여부
+    public bool waiting; // 대기 여부
+
+    // UI
+    public TMP_Text waveText;
+    public TMP_Text timeText;
+    public TMP_Text guidText;
+
+    public GameObject portal; // 클리어 포탈
+
+    void Update()
     {
-        wave = 0;
+        if (!start)
+        {
+            GameStart();
+        }
+
+        if (start && totalMonsterCount == 0)
+        {
+            StartCoroutine(WaitingStage());
+        }
     }
 
+    // 웨이브 당 소환할 몬스터 수 설정
     public void SummonCount()
     {
         switch (wave)
         {
             case 1:
                 creepCount = 10;
+                totalMonsterCount = 10;
                 break;
             case 2:
                 creepCount = 12;
                 demonCount = 3;
+                totalMonsterCount = 15;
                 break;
             case 3:
                 creepCount = 15;
                 demonCount = 5;
+                totalMonsterCount = 20;
                 break;
             case 4:
                 creepCount = 15;
                 demonCount = 10;
+                totalMonsterCount = 25;
                 break;
             case 5:
                 creepCount = 7;
                 demonCount = 3;
+                totalMonsterCount = 10;
                 break;
         }
+    }
+
+    // 시작시 30초후 시작
+    void GameStart()
+    {
+        if(gameTime < 30)
+        {
+            gameTime += Time.deltaTime;
+        }
+
+        if(gameTime >= 30)
+        {
+            SummonMonster();
+            gameTime = 0;
+            start = true; // 시작 여부 변경전 토탈 몬스터 수 변경 되는지 확인 필요
+        }
+    }
+
+    // 몬스터 소환
+    void SummonMonster()
+    {
+        SummonCount();
+        StartCoroutine(CreepSummon());
+        StartCoroutine(DemonSummon());
+    }
+    
+    IEnumerator CreepSummon()
+    {
+        for (int i = 0; i < creepCount; i++)
+        {
+            GameObject creep = summonManager.MonstetSummon(summonManager.creepQueue);
+            // 랜덤 위치 선택
+            Transform randomPosition = creepSummonPositions[Random.Range(0, creepSummonPositions.Length)];
+            // 몬스터 이동
+            creep.transform.position = randomPosition.position;
+
+            yield return new WaitForSeconds(3f);
+        }
+    }
+    IEnumerator DemonSummon()
+    {
+        for (int i = 0; i < demonCount; i++)
+        {
+            GameObject demon = summonManager.MonstetSummon(summonManager.demonQueue);
+            // 랜덤 위치 선택
+            Transform randomPosition = creepSummonPositions[Random.Range(0, demonSummonPositions.Length)];
+            // 몬스터 이동
+            demon.transform.position = randomPosition.position;
+
+            yield return new WaitForSeconds(3f);
+        }
+    }
+
+    // 몬스터 모두 사망시 20초 대기시간
+    IEnumerator WaitingStage()
+    {
+        yield return new WaitForSeconds(20f);
+
+        gameTime = 0;
+        SummonMonster();
+    }
+
+    // 모든 웨이브 클리어시 포탈생성
+    void GameClear()
+    {
+        portal.SetActive(true);
     }
 }
--- Techna/Assets/Scripts/Stage/Stage4/SummonManager.cs
+++ Techna/Assets/Scripts/Stage/Stage4/SummonManager.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class SummonManager : MonoBehaviour
 {
+    // 몬스터 프리팹
     public GameObject creep_Basic;
     public GameObject creep_Crouch;
     public GameObject demon_Basic;
     public GameObject demon_Attack;
     public GameObject boss;
 
-    private Queue<GameObject> creepQueue = new Queue<GameObject>();
-    private Queue<GameObject> demonQueue = new Queue<GameObject>();
+    // 몬스터 큐
+    public Queue<GameObject> creepQueue = new Queue<GameObject>();
+    public Queue<GameObject> demonQueue = new Queue<GameObject>();
     private Queue<GameObject> bossQueue = new Queue<GameObject>();
 
+    // 총 몬스터 수
     public int creep_Count = 59;
     public int d_BasicCount = 21;
     public int bossCount = 1;
@@ -23,6 +26,8 @@ public class SummonManager : MonoBehaviour
         SummonCreeps();
         SummonDemons();
     }
+
+    // 몬스터 생성 및 큐 저장
     private void SummonCreeps()
     {
         for (int i = 0; i < creep_Count; i++)
@@ -34,7 +39,6 @@ public class SummonManager : MonoBehaviour
             creepQueue.Enqueue(obj); // 큐에 추가
         }
     }
-
     private void SummonDemons()
     {
         for (int i = 0; i < d_BasicCount; i++)
@@ -46,6 +50,7 @@ public class SummonManager : MonoBehaviour
         }
     }
 
+    // 몬스터 소환
     public GameObject MonstetSummon(Queue<GameObject> pool)
     {
         if (pool.Count > 0)
@@ -55,11 +60,5 @@ public class SummonManager : MonoBehaviour
             return obj;
         }
         return null; // 풀에 오브젝트가 없을 경우
-    }
-
-    public void ReturnObjectToPool(Queue<GameObject> pool, GameObject obj)
-    {
-        obj.SetActive(false); // 비활성화
-        pool.Enqueue(obj);
     }
 }
